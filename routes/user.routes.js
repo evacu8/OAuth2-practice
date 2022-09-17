b647ac97dac2d897c9b899e7ad315ddf2867e712
Krzysztof Potocki <kpotocki89@gmail.com>
2022-09-17 14:27:08 +0200

    Add middleware for checking logged status

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/logged", (req, res) => {
-  if (req.user) {
-    res.render("logged");
-  } else {
+const isLogged = (req, res, next) => {
+  if (!req.user) {
     res.redirect("/user/no-permission");
+  } else {
+    next();
   }
+};
+
+router.get("/logged", isLogged, (req, res) => {
+  res.render("logged");
 });
 
-router.get("/profile", (req, res) => {
-  if (req.user) {
-    res.render("profile");
-  } else {
-    res.redirect("/user/no-permission");
-  }
+router.get("/profile", isLogged, (req, res) => {
+  res.render("profile");
 });
 
-router.get("/profile/settings", (req, res) => {
-  if (req.user) {
-    res.render("profileSettings");
-  } else {
-    res.redirect("/user/no-permission");
-  }
+router.get("/profile/settings", isLogged, (req, res) => {
+  res.render("profileSettings");
 });
 
 router.get("/no-permission", (req, res) => {
