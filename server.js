3eb4642098d607d8cf83a258f7b318024be63c33
Krzysztof Potocki <kpotocki89@gmail.com>
2022-09-17 13:22:24 +0200

    Add authentication with passport

--- server.js
+++ server.js
@@ -1,34 +1,57 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const hbs = require('express-handlebars');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const hbs = require("express-handlebars");
+const passport = require("passport");
+const passportConfig = require("./config/passport");
+const session = require("express-session");
 
 const app = express();
 
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
-app.set('view engine', '.hbs');
+app.engine(
+  "hbs",
+  hbs({ extname: "hbs", layoutsDir: "./layouts", defaultLayout: "main" })
+);
+app.set("view engine", ".hbs");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.use(session({ secret: "anything" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get("/user/logged", (req, res) => {
+  res.render("logged");
 });
 
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
+app.get("/user/no-permission", (req, res) => {
+  res.render("noPermission");
 });
 
-app.use('/', (req, res) => {
-  res.status(404).render('notFound');
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/user/no-permission" }),
+  (req, res) => {
+    res.redirect("/user/logged");
+  }
+);
+
+app.use("/", (req, res) => {
+  res.status(404).render("notFound");
 });
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+app.listen("8000", () => {
+  console.log("Server is running on port: 8000");
 });
